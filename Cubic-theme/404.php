9d9a7601ec718782cdb9598af31e582f1186e4ba
andresknoo00001 <32203440+andresknoo00001@users.noreply.github.com>
2021-12-12 16:21:24 -0500

    Add files via upload

---
+++ Cubic-theme/404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<section id="container-wrap">
+	<section id="error404">
+		<div class="container-row">
+			<div class="content-page" style="text-align:center;">
+				<h1>ERROR <span class="big">404</span></h1>
+				<p style="text-align:center;">Estás intentando ingresar a una <strong>URL</strong> que no existe.</p>
+				<a href="<?php echo get_home_url(); ?>" class="button-default">Volver al inicio</a>
+			</div>
+		</div>
+	</section>
+</section>
+
+<?php get_footer(); ?>
